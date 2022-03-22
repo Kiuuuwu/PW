ee88dd4aa0a14c47354157afb9319669b9bc78fa
Kiuuuwu <frykowska.ka@gmail.com>
2022-03-22 21:51:51 +0100

    added methods: Add, Substract, Multiply, Divide in Calculator class

---
+++ TestProject_PW/UnitTest1.cs
@@ -0,0 +1,61 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorNS;
+
+namespace TestProject_PW
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_WithValidInput_ReturnsSum()
+        {
+            double number = 1.5;
+            double number1 = 2;
+            double expected = 3.5;
+            Calculator calculator = new Calculator();
+
+            double actual = calculator.Add(number, number1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Substract_WithValidInput_ReturnsSubstraction()
+        {
+            double number = 1.5;
+            double number1 = 2;
+            double expected = -0.5;
+            Calculator calculator = new Calculator();
+
+            double actual = calculator.Substract(number, number1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Multiply_WithValidInput_ReturnsMultiplication()
+        {
+            double number = 1.5;
+            double number1 = 2;
+            double expected = 3;
+            Calculator calculator = new Calculator();
+
+            double actual = calculator.Multiply(number, number1);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Divide_WithValidInput_ReturnsDivision()
+        {
+            double number = 1.5;
+            double number1 = 2;
+            double expected = 0.75;
+            Calculator calculator = new Calculator();
+
+            double actual = calculator.Divide(number, number1);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- wspolbiezne1/Program.cs
+++ wspolbiezne1/Program.cs
@@ -1,1 +1,44 @@
-﻿int outcome = 2 + 2 * 2;
+﻿using System;
+
+namespace CalculatorNS
+{
+    public class Calculator
+    {
+        private double c_number;
+        private double c_number1;
+
+        public Calculator() {}
+
+        public double Add(double number, double number1)
+        {
+            return number + number1;
+        }
+
+        public double Substract(double number, double number1)
+        {
+            return number - number1;
+        }
+
+        public double Multiply(double number, double number1)
+        {
+            return number * number1;
+        }
+
+        public double Divide(double number, double number1)
+        {
+            return number / number1;
+        }
+
+        public static void Main()
+        {
+            double number = 2;
+            double number1 = 7;
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("add(" + number + ", " + number1 + "): " + calculator.Add(number, number1));
+            Console.WriteLine("substract(" + number + ", " + number1 + "): " + calculator.Substract(number, number1));
+            Console.WriteLine("multiply(" + number + ", " + number1 + "): " + calculator.Multiply(number, number1));
+            Console.WriteLine("divide(" + number + ", " + number1 + "): " + calculator.Divide(number, number1));
+        }
+    }
+}
