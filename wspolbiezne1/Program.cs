2e8a0619c084ea35a76824b5844371f42dbe157f
Kiuuuwu <frykowska.ka@gmail.com>
2022-03-24 20:52:33 +0100

    refactored code

--- wspolbiezne1/Program.cs
+++ wspolbiezne1/Program.cs
@@ -4,8 +4,6 @@ namespace CalculatorNS
 {
     public class Calculator
     {
-        private double c_number;
-        private double c_number1;
 
         public Calculator() {}
 
@@ -31,30 +29,30 @@ namespace CalculatorNS
 
         public static void Main()
         {
-            Console.WriteLine("Please specify two numbers you would like to:\nadd, subtract, multiply or divide:" +
+            Console.WriteLine("Please specify two numbers you would like to add, subtract, multiply or divide:" +
                 "\nPlease type in the first number");
 
-            double number = Convert.ToDouble(Console.ReadLine());   // note: in c# we use ',', not '.' to create double
+            double number = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Please type in the second number");
             double number1 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Please specify operation:\n[1] add\n[2] subtract\n[3] multiply\n[4] divide:");
-            string operationNumber = Console.ReadLine();
+            string? operationNumber = Console.ReadLine();
             Calculator calculator = new Calculator();
 
             switch (operationNumber)
             {
                 case "1":
-                    Console.WriteLine("add(" + number + ", " + number1 + "): " + calculator.Add(number, number1));
+                    Console.WriteLine(number + " + " + number1 + " = " + calculator.Add(number, number1));
                     break;
                 case "2":
-                    Console.WriteLine("subtract(" + number + ", " + number1 + "): " + calculator.Subtract(number, number1));
+                    Console.WriteLine(number + " - " + number1 + " = " + calculator.Subtract(number, number1));
                     break;
                 case "3":
-                    Console.WriteLine("multiply(" + number + ", " + number1 + "): " + calculator.Multiply(number, number1));
+                    Console.WriteLine(number + " * " + number1 + " = " + calculator.Multiply(number, number1));
                     break;
                 case "4":
-                    Console.WriteLine("divide(" + number + ", " + number1 + "): " + calculator.Divide(number, number1));
+                    Console.WriteLine(number + " / " + number1 + " = " + calculator.Divide(number, number1));
                     break;
             }  
         }
